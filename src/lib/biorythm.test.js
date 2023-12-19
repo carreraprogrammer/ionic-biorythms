2a00a785b1e4d4fd70dc42e80a9ef42a1281b0a6
Daniel <carreraprogrammer@gmail.com>
2023-12-19 07:49:46 -0500

    Add biorhythms calculation functionality

--- src/lib/biorythm.test.js
+++ src/lib/biorythm.test.js
@@ -1,4 +1,4 @@
-import { calculateBiorhythms } from './biorhythm';
+import { calculateBiorhythms } from './biorhythms';
 
 it('calculates the physical biorhythm', () => {
   const biorhythms = calculateBiorhythms('1999-12-31', '2022-12-31');
