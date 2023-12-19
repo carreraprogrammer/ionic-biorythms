fa930c28ca09047bc113e2a46a74b8102b84036a
Daniel <carreraprogrammer@gmail.com>
2023-12-19 07:18:12 -0500

    Add dayjs library and BiorhythmCard component

--- src/components/BiorhythmCard.js
+++ src/components/BiorhythmCard.js
@@ -1,10 +1,16 @@
 import { IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
+import dayjs from 'dayjs';
+
+const formatDate = (isoString) => {
+  return dayjs(isoString).format('D MMM YYYY');
+};
 
 function BiorhythmCard({ targetDate }) {
+
   return (
     <IonCard className="ion-text-center">
       <IonCardHeader>
-        <IonCardTitle>{targetDate}</IonCardTitle>
+        <IonCardTitle>{formatDate(targetDate)}</IonCardTitle>
       </IonCardHeader>
       <IonCardContent>
         <p>Physical: ???</p>
